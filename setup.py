14df79cb8f75a5ac82f6ebffd86094b8fd6338f7
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2019-07-31 23:06:01 +0400

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,28 @@
-from setuptools import find_packages, setup
+import setuptools
 
-with open('README.md', 'r') as fp:
-    long_description = fp.read()
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
-setup(
+setuptools.setup(
     name='simple-json-logging',
-    packages=find_packages(),
-    license='MIT',
     version='1.0.4',
+    license='MIT',
     author='Sergey Nevmerzhitsky',
     author_email='sergey.nevmerzhitsky@gmail.com',
-    url='https://github.com/nevmerzhitsky/python-simple-json-logging',
-    download_url='https://github.com/nevmerzhitsky/python-simple-json-logging/archive/v1.0.4.tar.gz',
     description='Library for structured logging via JSON document',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    url='https://github.com/nevmerzhitsky/python-simple-json-logging',
+    keywords=['logging', 'json'],
     install_requires=[
         'pytest>=3.7.0'
     ],
-    keywords=['logging', 'json'],
+    packages=setuptools.find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: OS Independent',
         'Topic :: Utilities',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
