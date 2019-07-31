c29ea4f1f259900691bd0b200bcb59333cb90e7e
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2019-07-31 22:25:09 +0400

    Switch to 1.0.2

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import find_packages, setup
 
+with open('README.md', 'r') as fp:
+    long_description = fp.read()
+
 setup(
     name='simple-json-logging',
     packages=find_packages(),
     license='MIT',
-    version='1.0.1',
+    version='1.0.2',
     author='Sergey Nevmerzhitsky',
     author_email='sergey.nevmerzhitsky@gmail.com',
     url='https://github.com/nevmerzhitsky/python-simple-json-logging',
-    download_url='https://github.com/nevmerzhitsky/python-simple-json-logging/archive/v1.0.1.tar.gz',
+    download_url='https://github.com/nevmerzhitsky/python-simple-json-logging/archive/v1.0.2.tar.gz',
     description='Library for structured logging via JSON document',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'pytest>=3.7.0'
     ],
