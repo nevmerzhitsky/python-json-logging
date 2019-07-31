af6add51b9bbc3ce64ed6d181558cf1169607abc
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2019-07-31 22:05:13 +0400

    Rename a package

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.md', 'r') as fp:
     long_description = fp.read()
 
 setup(
-    name='json-logging',
+    name='simple-json-logging',
     packages=find_packages(),
     license='MIT',
     version='1.0.0',
     author='Sergey Nevmerzhitsky',
     author_email='sergey.nevmerzhitsky@gmail.com',
-    url='https://github.com/nevmerzhitsky/python-json-logging',
-    download_url='https://github.com/nevmerzhitsky/python-json-logging/archive/v1.0.0.tar.gz',
+    url='https://github.com/nevmerzhitsky/python-simple-json-logging',
+    download_url='https://github.com/nevmerzhitsky/python-simple-json-logging/archive/v1.0.0.tar.gz',
     description='Library for structured logging via JSON document',
     long_description=long_description,
     long_description_content_type='text/markdown',
