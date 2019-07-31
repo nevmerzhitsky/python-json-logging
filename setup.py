087d2181eded4d2d19229c11a0be7a70b3244fc9
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2019-07-31 21:20:55 +0400

    Add setup config

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+install_requires = ['pytest>=3.7.0']
+
+with open('README.md', 'r') as fp:
+    long_description = fp.read()
+
+setup(
+    name='json-logging',
+    packages=find_packages(),
+    license='MIT',
+    version='1.0.0',
+    author='Sergey Nevmerzhitsky',
+    author_email='sergey.nevmerzhitsky@gmail.com',
+    url='https://github.com/nevmerzhitsky/python-json-logging',
+    description='Library for structured logging via JSON document',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
+    keywords=['logging', 'json'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
